170a194b027c8bc13f1aced11088423662482b09
MOBUMIN <kiju23@naver.com>
2021-02-09 22:17:34 +0900

    Header btn , Side bar
    * header에 들어갈 버튼 제작
    * quiz 번호를 선택할 sidebar 틀 제작
    * createroom에서 사용할 createroom.scss 작성

--- src/components/page/CreateRoom.js
+++ src/components/page/CreateRoom.js
@@ -1,12 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Header } from '../UI';
+import { Button, Drawer, List, ListItem, Toolbar } from '@material-ui/core';
+import { Link } from 'react-router-dom';
 
 const CreateRoom = () => {
+	const list = () => (
+		<List>
+			<ListItem>Hi</ListItem>
+		</List>
+	);
 	return (
-		<>
-			<Header left="left-1" right="right1" />
-			<div>create</div>
-		</>
+		<div className="CreateRoom">
+			<Header
+				left={
+					<Button variant="contained" color="primary" className="btn">
+						Settings
+					</Button>
+				}
+				right={
+					<>
+						<Button
+							variant="contained"
+							color="default"
+							className={`${'btn'} ${'rightbtn'}`}
+							component={Link}
+							to="/list"
+						>
+							EXIT
+						</Button>
+						<Button
+							variant="contained"
+							color="secondary"
+							className={`${'btn'} ${'rightbtn'}`}
+						>
+							DONE
+						</Button>
+					</>
+				}
+			/>
+			<Drawer
+				className="sidebar"
+				variant="permanent"
+				classes={{
+					paper: 'drawerPaper',
+				}}
+			>
+				<Toolbar />
+				{list()}
+			</Drawer>
+		</div>
 	);
 };
 
