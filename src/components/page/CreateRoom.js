12b46b8d394f8522812c91e16be44d8862c1597e
MOBUMIN <kiju23@naver.com>
2021-02-12 16:09:16 +0900

    Quiz front 구현
    * 정답 버튼부분 구현
    * container안에 item으로 넣기
    * header의 z-index 조정
    * sidebar의 z-index 조정
    ---
    To do
    * Toggle state & quiz/True or false state와 조건부 렌더링 이용하여 다르게 보여주기 ( 할 수 있을까.. ?)
    * ans paper 색깔 주기
    * 전체적으로 state 관리
    * option 칸 가운데 정렬로 바꾸기

--- src/components/page/CreateRoom.js
+++ src/components/page/CreateRoom.js
@@ -13,8 +13,11 @@ import {
 	FormControl,
 	MenuItem,
 	Typography,
+	Checkbox,
 } from '@material-ui/core';
 import StarsIcon from '@material-ui/icons/Stars';
+import CircleCheckedFilled from '@material-ui/icons/CheckCircle';
+import CircleUnchecked from '@material-ui/icons/RadioButtonUnchecked';
 import { Link } from 'react-router-dom';
 
 const CreateRoom = () => {
@@ -64,6 +67,7 @@ const CreateRoom = () => {
 			>
 				<Toolbar />
 				{list()}
+				<Button variant="contained">Add question</Button>
 			</Drawer>
 			{/* Main */}
 			<div className="main">
@@ -89,62 +93,104 @@ const CreateRoom = () => {
 								</Paper>
 							</Grid>
 							{/* 퀴즈 옵션 설정하기 */}
-							<Grid item xs={12} className="optContainer">
-								<div className="circleDiv">
-									<Typography className="circleText">Time Limit</Typography>
-									<Paper className="circle">
-										<FormControl>
-											<Select className="selectOpt">
-												<MenuItem value={20}>20 seconds</MenuItem>
-											</Select>
-										</FormControl>
-									</Paper>
-								</div>
-								<div className="circleDiv">
-									<Typography className="circleText">Points</Typography>
-									<Paper className="circle">
-										<FormControl>
-											<Select className="selectOpt">
-												<MenuItem value={20}>20 seconds</MenuItem>
-											</Select>
-										</FormControl>
-									</Paper>
-								</div>
-								<div className="circleDiv">
-									<Typography className="circleText">Answer Opt</Typography>
-									<Paper className="circle">
-										<FormControl>
-											<Select className="selectOpt">
-												<MenuItem value={20}>20 seconds</MenuItem>
-											</Select>
-										</FormControl>
-									</Paper>
-								</div>
+							<Grid container className="optContainer">
+								<Grid item xs={4} className="circleItem">
+									<div className="circleDiv">
+										<Typography className="circleText">Time Limit</Typography>
+										<Paper className="circle">
+											<FormControl>
+												<Select className="selectOpt" defaultValue={20}>
+													<MenuItem value={20}>20 seconds</MenuItem>
+												</Select>
+											</FormControl>
+										</Paper>
+									</div>
+								</Grid>
+								<Grid item xs={4} className="circleItem">
+									<div className="circleDiv">
+										<Typography className="circleText">Points</Typography>
+										<Paper className="circle">
+											<FormControl>
+												<Select className="selectOpt" defaultValue={20}>
+													<MenuItem value={20}>20 seconds</MenuItem>
+												</Select>
+											</FormControl>
+										</Paper>
+									</div>
+								</Grid>
+								<Grid item xs={4} className="circleItem">
+									<div className="circleDiv">
+										<Typography className="circleText">Answer Opt</Typography>
+										<Paper className="circle">
+											<FormControl>
+												<Select className="selectOpt" defaultValue={20}>
+													<MenuItem value={20}>20 seconds</MenuItem>
+												</Select>
+											</FormControl>
+										</Paper>
+									</div>
+								</Grid>
 							</Grid>
 							{/* 정답 설정하기 */}
 							<Grid container>
-								<Grid item xs={6}>
-									<Paper elevation={0}>
-										<StarsIcon />
-										<InputBase className="ansInput" placeholder="Add Answer" />
+								<Grid item xs={6} className="ansItem">
+									<Paper elevation={0} className="ansPaper">
+										<StarsIcon className="ansInput" />
+										<InputBase
+											className="ansInput"
+											placeholder="Add Answer"
+											fullWidth
+										/>
+										<Checkbox
+											checked={true}
+											icon={<CircleUnchecked className="ansInput" />}
+											checkedIcon={<CircleCheckedFilled className="ansInput" />}
+										/>
 									</Paper>
 								</Grid>
-								<Grid item xs={6}>
-									<Paper elevation={0}>
-										<StarsIcon />
-										<InputBase className="ansInput" placeholder="Add Answer" />
+								<Grid item xs={6} className="ansItem">
+									<Paper elevation={0} className="ansPaper">
+										<StarsIcon className="ansInput" />
+										<InputBase
+											className="ansInput"
+											placeholder="Add Answer"
+											fullWidth
+										/>
+										<Checkbox
+											checked={true}
+											icon={<CircleUnchecked className="ansInput" />}
+											checkedIcon={<CircleCheckedFilled className="ansInput" />}
+										/>
 									</Paper>
 								</Grid>
-								<Grid item xs={6}>
-									<Paper elevation={0}>
-										<StarsIcon />
-										<InputBase className="ansInput" placeholder="Add Answer" />
+								<Grid item xs={6} className="ansItem">
+									<Paper elevation={0} className="ansPaper">
+										<StarsIcon className="ansInput" />
+										<InputBase
+											className="ansInput"
+											placeholder="Add Answer"
+											fullWidth
+										/>
+										<Checkbox
+											checked={true}
+											icon={<CircleUnchecked className="ansInput" />}
+											checkedIcon={<CircleCheckedFilled className="ansInput" />}
+										/>
 									</Paper>
 								</Grid>
-								<Grid item xs={6}>
-									<Paper elevation={0}>
-										<StarsIcon />
-										<InputBase className="ansInput" placeholder="Add Answer" />
+								<Grid item xs={6} className="ansItem">
+									<Paper elevation={0} className="ansPaper">
+										<StarsIcon className="ansInput" />
+										<InputBase
+											className="ansInput"
+											placeholder="Add Answer"
+											fullWidth
+										/>
+										<Checkbox
+											checked={true}
+											icon={<CircleUnchecked className="ansInput" />}
+											checkedIcon={<CircleCheckedFilled className="ansInput" />}
+										/>
 									</Paper>
 								</Grid>
 							</Grid>
