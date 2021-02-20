c564947aed607cf6c53012f3758ed3af50609cf6
MOBUMIN <kiju23@naver.com>
2021-02-20 23:42:31 +0900

    TF Layout 구현
    * QuizLayout의 map함수만 제거하여 TFLayout 구현
    
    To do
    * TF CSS 조정
    * state 관리는 어떻게 .. ?
    * ansPaper의 check icon 색변경 + border주기

--- src/components/UI/CreateRoomUI.js
+++ src/components/UI/CreateRoomUI.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
 	Toolbar,
 	InputBase,
@@ -111,12 +111,109 @@ function QuizLayout() {
 }
 
 function TFLayout() {
-	return <div></div>;
+	return (
+		<Grid container>
+			<Grid item xs>
+				{/* main 위치조정 */}
+				<Toolbar />
+			</Grid>
+			<Grid item xs={10}>
+				<Grid container className="mainContainer">
+					{/* Title 입력 칸 */}
+					<Grid item xs={12}>
+						<Paper component="form" className="inputPaper">
+							<InputBase
+								className="inputTitle"
+								placeholder="Click to start typing your question"
+								inputProps={{
+									maxLength: 110,
+									style: { textAlign: 'center' },
+								}}
+								multiline
+							/>
+						</Paper>
+					</Grid>
+					{/* 퀴즈 옵션 설정하기 */}
+					<Grid container className="optContainer">
+						<Grid item xs={4} className="circleItem">
+							<div className="circleDiv">
+								<Typography className="circleText">Time Limit</Typography>
+								<Paper className="circle">
+									<FormControl>
+										<Select
+											className="selectOpt"
+											defaultValue={20}
+											classes={{ icon: 'selectIcon' }}
+										>
+											<MenuItem value={20}>20 seconds</MenuItem>
+										</Select>
+									</FormControl>
+								</Paper>
+							</div>
+						</Grid>
+						<Grid item xs={4} className="circleItem">
+							<div className="circleDiv">
+								<Typography className="circleText">Points</Typography>
+								<Paper className="circle">
+									<FormControl>
+										<Select
+											className="selectOpt"
+											defaultValue={20}
+											classes={{ icon: 'selectIcon' }}
+										>
+											<MenuItem value={20}>standard</MenuItem>
+										</Select>
+									</FormControl>
+								</Paper>
+							</div>
+						</Grid>
+					</Grid>
+					{/* 정답 설정하기 */}
+					<Grid container>
+						<Grid item xs={6} className="ansItem">
+							<Paper elevation={0} className={`${'ansPaper'} ${'ansblue'}`}>
+								<StarsIcon className="ansInput" />
+								<InputBase
+									className="ansInput"
+									placeholder="Add Answer"
+									fullWidth
+								/>
+								<Checkbox
+									checked={true}
+									icon={<CircleUnchecked className="ansInput" />}
+									checkedIcon={<CircleCheckedFilled className="ansInput" />}
+								/>
+							</Paper>
+						</Grid>
+						<Grid item xs={6} className="ansItem">
+							<Paper elevation={0} className={`${'ansPaper'} ${'ansred'}`}>
+								<StarsIcon className="ansInput" />
+								<InputBase
+									className="ansInput"
+									placeholder="Add Answer"
+									fullWidth
+								/>
+								<Checkbox
+									checked={true}
+									icon={<CircleUnchecked className="ansInput" />}
+									checkedIcon={<CircleCheckedFilled className="ansInput" />}
+								/>
+							</Paper>
+						</Grid>
+					</Grid>
+				</Grid>
+			</Grid>
+		</Grid>
+	);
 }
 
 function CreateRoomQuiz(props) {
 	const toggleId = props.toggleId;
 	const quizlist = props.quizlist;
+	// const [ansData, setansData] = useState([
+	// 	{ 0: true, 1: false, 2: false, 3: false },
+	// 	{ 0: true, 1: false },
+	// ]);
 	if (quizlist[toggleId].sort === 'Quiz') return <QuizLayout />;
 	else return <TFLayout />;
 }
